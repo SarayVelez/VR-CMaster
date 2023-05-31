df1a225faac23a57a1367185690db1e4b1001cb4
Alejandro Rodriguez <alejandro.rodriguez2899@gmail.com>
2023-05-31 13:37:26 -0600

    Correccion de collisiones
    Sigue sin funcionar no me obtiene la referencia de los triggers de los sockets

--- Assets/Scripts/CollisionChecker.cs
+++ Assets/Scripts/CollisionChecker.cs
@@ -4,132 +4,96 @@ using UnityEngine;
 using UnityEngine.UI;
 public class CollisionChecker : MonoBehaviour
 {
-    /*public RAMSocketColliderUno ramSocketColliderUno;
-    public RAMSocketColliderDos ramSocketColliderDos;
-    public PROCSocketCollider procSocketCollider;
-    public HDDSocketCollider hddSocketCollider;
-    public FUENTESocketCollider fuenteSocketCollider;
-    public GPUSocketCollider gpuSocketCollider;
-    public MOTHERSocketCollider motherSocketCollider;
-    public Text resultText;*/
-
     public Text resultText;
 
-    private List<MonoBehaviour> collisionScripts = new List<MonoBehaviour>();
-
+    public bool isRamUnoEncajada = false;
+    public bool isRamDosEncajada = false;
+    public bool isProc1Encajado = false;
+    public bool isProc2Encajado = false;
+    public bool isGpuEncajado = false;
+    public bool isMotEncajado = false;
+    public bool isFontEncajado = false;
+    public bool isDiscEncajado = false;
     private void Start()
     {
-        CollectCollisionScripts();
+        // Obtener referencia al objeto socket
+        GameObject socketuno = GameObject.Find("Socket");
+
+        // Obtener referencia al script RAMSocketCollider
+        RAMSocketColliderUno rAMSocketColliderUno = socketuno.GetComponent<RAMSocketColliderUno>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isRamUnoEncajada = rAMSocketColliderUno.ramunoEncajada;
+        
+        // Obtener referencia al objeto socket
+        GameObject socketdos = GameObject.Find("Socket");
+
+        // Obtener referencia al script RAMSocketCollider
+        RAMSocketColliderDos ramSocketColliderDos = socketdos.GetComponent<RAMSocketColliderDos>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isRamDosEncajada = ramSocketColliderDos.ramdosEncajada;
+
+        // Obtener referencia al objeto socket
+        GameObject sockettres = GameObject.Find("Socket");
+
+        // Obtener referencia al script RAMSocketCollider
+        PROCSocketCollider procSocketCollider = sockettres.GetComponent<PROCSocketCollider>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isProc1Encajado = procSocketCollider.proc1Encajado;
+        // Acceder a la variable pública ramdosEncajada
+        isProc2Encajado = procSocketCollider.proc2Encajado;
+
+         // Obtener referencia al objeto socket
+        GameObject socketcuatro = GameObject.Find("Socket");
+
+        // Obtener referencia al script RAMSocketCollider
+        GPUSocketCollider gpuSocketCollider = socketcuatro.GetComponent<GPUSocketCollider>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isGpuEncajado = gpuSocketCollider.gpuEncajada;
+
+        // Obtener referencia al objeto socket
+        GameObject socketcinco = GameObject.Find("Socket");
+
+        // Obtener referencia al script RAMSocketCollider
+        MOTHERSocketCollider motherSocketCollider = socketcinco.GetComponent<MOTHERSocketCollider>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isMotEncajado = motherSocketCollider.motEncajada;
+
+        // Obtener referencia al objeto socket
+        GameObject socketseis = GameObject.Find("SocketFuente");
+
+        // Obtener referencia al script RAMSocketCollider
+        FUENTESocketCollider fuenteSocketCollider = socketseis.GetComponent<FUENTESocketCollider>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isFontEncajado = fuenteSocketCollider.fontEncajada;
+
+        // Obtener referencia al objeto socket
+        GameObject socketsiete = GameObject.Find("SocketDisco");
+
+        // Obtener referencia al script RAMSocketCollider
+        HDDSocketCollider hddSocketCollider = socketsiete.GetComponent<HDDSocketCollider>();
+
+        // Acceder a la variable pública ramdosEncajada
+        isDiscEncajado = hddSocketCollider.hddEncajado;
     }
 
     private void Update()
     {
-        bool allCollisions = CheckAllCollisions();
-
+        bool allCollisions = (isRamUnoEncajada || isRamDosEncajada) && (isProc1Encajado || isProc2Encajado) 
+                        && isGpuEncajado && isMotEncajado && isFontEncajado && isDiscEncajado;
         // Actualizar el texto según el resultado de la comprobación
         if (allCollisions)
         {
-            resultText.text = "Todas las piezas están encajadas correctamente";
+            resultText.text = "Alta";
         }
         else
         {
-            resultText.text = "Algunas piezas no están encajadas correctamente";
+            resultText.text = "N/A";
         }
-    }
-
-    private void CollectCollisionScripts()
-    {
-        RAMSocketColliderUno[] ramCollidersUno = FindObjectsOfType<RAMSocketColliderUno>();
-        foreach (RAMSocketColliderUno collider in ramCollidersUno)
-        {
-            collisionScripts.Add(collider);
-        }
-
-        RAMSocketColliderDos[] ramCollidersDos = FindObjectsOfType<RAMSocketColliderDos>();
-        foreach (RAMSocketColliderDos collider in ramCollidersDos)
-        {
-            collisionScripts.Add(collider);
-        }
-
-        PROCSocketCollider[] procColliders = FindObjectsOfType<PROCSocketCollider>();
-        foreach (PROCSocketCollider collider in procColliders)
-        {
-            collisionScripts.Add(collider);
-        }
-
-        HDDSocketCollider[] hddColliders = FindObjectsOfType<HDDSocketCollider>();
-        foreach (HDDSocketCollider collider in hddColliders)
-        {
-            collisionScripts.Add(collider);
-        }
-
-        FUENTESocketCollider[] fuenteColliders = FindObjectsOfType<FUENTESocketCollider>();
-        foreach (FUENTESocketCollider collider in fuenteColliders)
-        {
-            collisionScripts.Add(collider);
-        }
-
-        GPUSocketCollider[] gpuColliders = FindObjectsOfType<GPUSocketCollider>();
-        foreach (GPUSocketCollider collider in gpuColliders)
-        {
-            collisionScripts.Add(collider);
-        }
-
-        MOTHERSocketCollider[] motherColliders = FindObjectsOfType<MOTHERSocketCollider>();
-        foreach (MOTHERSocketCollider collider in motherColliders)
-        {
-            collisionScripts.Add(collider);
-        }
-    }
-
-    private bool CheckAllCollisions()
-    {
-        bool proc1Encajado = false;
-        bool proc2Encajado = false;
-        bool ram1Encajada = false;
-        bool ram2Encajada = false;
-        bool hddEncajado = false;
-        bool fuenteEncajada = false;
-        bool gpuEncajada = false;
-        bool motherEncajada = false;
-
-        // Revisa todos los scripts de colisión almacenados en la lista
-        foreach (MonoBehaviour collisionScript in collisionScripts)
-        {
-            if (collisionScript is RAMSocketColliderUno)
-            {
-                ram1Encajada = ((RAMSocketColliderUno)collisionScript).ramunoEncajada;
-            }
-            else if (collisionScript is RAMSocketColliderDos)
-            {
-                ram2Encajada = ((RAMSocketColliderDos)collisionScript).ramdosEncajada;
-            }
-            else if (collisionScript is PROCSocketCollider)
-            {
-                proc1Encajado = ((PROCSocketCollider)collisionScript).proc1Encajado;
-                proc2Encajado = ((PROCSocketCollider)collisionScript).proc2Encajado;
-            }
-            else if (collisionScript is HDDSocketCollider)
-            {
-                hddEncajado = ((HDDSocketCollider)collisionScript).hddEncajado;
-            }
-            else if (collisionScript is FUENTESocketCollider)
-            {
-                fuenteEncajada = ((FUENTESocketCollider)collisionScript).fontEncajada;
-            }
-            else if (collisionScript is GPUSocketCollider)
-            {
-                gpuEncajada = ((GPUSocketCollider)collisionScript).gpuEncajada;
-            }
-            else if (collisionScript is MOTHERSocketCollider)
-            {
-                motherEncajada = ((MOTHERSocketCollider)collisionScript).motEncajada;
-            }
-        }
-
-        // Realiza las comprobaciones necesarias
-        bool allCollisions = proc1Encajado && !proc2Encajado && ram1Encajada && ram2Encajada && hddEncajado && fuenteEncajada && gpuEncajada && motherEncajada;
-
-        return allCollisions;
     }
 }
