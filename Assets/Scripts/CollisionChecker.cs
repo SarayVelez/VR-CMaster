7777287b67cf31d20a517a29f6035b340887ec87
Alejandro Rodriguez <alejandro.rodriguez2899@gmail.com>
2023-05-31 10:59:32 -0600

    Correccion de socket procesador

--- Assets/Scripts/CollisionChecker.cs
+++ Assets/Scripts/CollisionChecker.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 public class CollisionChecker : MonoBehaviour
 {
-    public RAMSocketColliderUno ramSocketColliderUno;
+    /*public RAMSocketColliderUno ramSocketColliderUno;
     public RAMSocketColliderDos ramSocketColliderDos;
     public PROCSocketCollider procSocketCollider;
     public HDDSocketCollider hddSocketCollider;
     public FUENTESocketCollider fuenteSocketCollider;
     public GPUSocketCollider gpuSocketCollider;
     public MOTHERSocketCollider motherSocketCollider;
+    public Text resultText;*/
 
     public Text resultText;
 
+    private List<MonoBehaviour> collisionScripts = new List<MonoBehaviour>();
+
+    private void Start()
+    {
+        CollectCollisionScripts();
+    }
+
     private void Update()
     {
-        bool allCollisions = (ramSocketColliderUno.ramunoEncajada || ramSocketColliderDos.ramdosEncajada) &&
-                         (procSocketCollider.proc1Encajado || procSocketCollider.proc2Encajado) &&
-                         hddSocketCollider.hddEncajado &&
-                         fuenteSocketCollider.fontEncajada &&
-                         gpuSocketCollider.gpuEncajada &&
-                         motherSocketCollider.motEncajada;
+        bool allCollisions = CheckAllCollisions();
 
         // Actualizar el texto según el resultado de la comprobación
         if (allCollisions)
@@ -32,5 +35,101 @@ public class CollisionChecker : MonoBehaviour
         {
             resultText.text = "Algunas piezas no están encajadas correctamente";
         }
+    }
+
+    private void CollectCollisionScripts()
+    {
+        RAMSocketColliderUno[] ramCollidersUno = FindObjectsOfType<RAMSocketColliderUno>();
+        foreach (RAMSocketColliderUno collider in ramCollidersUno)
+        {
+            collisionScripts.Add(collider);
+        }
+
+        RAMSocketColliderDos[] ramCollidersDos = FindObjectsOfType<RAMSocketColliderDos>();
+        foreach (RAMSocketColliderDos collider in ramCollidersDos)
+        {
+            collisionScripts.Add(collider);
+        }
+
+        PROCSocketCollider[] procColliders = FindObjectsOfType<PROCSocketCollider>();
+        foreach (PROCSocketCollider collider in procColliders)
+        {
+            collisionScripts.Add(collider);
+        }
+
+        HDDSocketCollider[] hddColliders = FindObjectsOfType<HDDSocketCollider>();
+        foreach (HDDSocketCollider collider in hddColliders)
+        {
+            collisionScripts.Add(collider);
+        }
+
+        FUENTESocketCollider[] fuenteColliders = FindObjectsOfType<FUENTESocketCollider>();
+        foreach (FUENTESocketCollider collider in fuenteColliders)
+        {
+            collisionScripts.Add(collider);
+        }
+
+        GPUSocketCollider[] gpuColliders = FindObjectsOfType<GPUSocketCollider>();
+        foreach (GPUSocketCollider collider in gpuColliders)
+        {
+            collisionScripts.Add(collider);
+        }
+
+        MOTHERSocketCollider[] motherColliders = FindObjectsOfType<MOTHERSocketCollider>();
+        foreach (MOTHERSocketCollider collider in motherColliders)
+        {
+            collisionScripts.Add(collider);
+        }
+    }
+
+    private bool CheckAllCollisions()
+    {
+        bool proc1Encajado = false;
+        bool proc2Encajado = false;
+        bool ram1Encajada = false;
+        bool ram2Encajada = false;
+        bool hddEncajado = false;
+        bool fuenteEncajada = false;
+        bool gpuEncajada = false;
+        bool motherEncajada = false;
+
+        // Revisa todos los scripts de colisión almacenados en la lista
+        foreach (MonoBehaviour collisionScript in collisionScripts)
+        {
+            if (collisionScript is RAMSocketColliderUno)
+            {
+                ram1Encajada = ((RAMSocketColliderUno)collisionScript).ramunoEncajada;
+            }
+            else if (collisionScript is RAMSocketColliderDos)
+            {
+                ram2Encajada = ((RAMSocketColliderDos)collisionScript).ramdosEncajada;
+            }
+            else if (collisionScript is PROCSocketCollider)
+            {
+                proc1Encajado = ((PROCSocketCollider)collisionScript).proc1Encajado;
+                proc2Encajado = ((PROCSocketCollider)collisionScript).proc2Encajado;
+            }
+            else if (collisionScript is HDDSocketCollider)
+            {
+                hddEncajado = ((HDDSocketCollider)collisionScript).hddEncajado;
+            }
+            else if (collisionScript is FUENTESocketCollider)
+            {
+                fuenteEncajada = ((FUENTESocketCollider)collisionScript).fontEncajada;
+            }
+            else if (collisionScript is GPUSocketCollider)
+            {
+                gpuEncajada = ((GPUSocketCollider)collisionScript).gpuEncajada;
+            }
+            else if (collisionScript is MOTHERSocketCollider)
+            {
+                motherEncajada = ((MOTHERSocketCollider)collisionScript).motEncajada;
+            }
+        }
+
+        // Realiza las comprobaciones necesarias
+        bool allCollisions = proc1Encajado && !proc2Encajado && ram1Encajada && ram2Encajada && hddEncajado && fuenteEncajada && gpuEncajada && motherEncajada;
+
+        return allCollisions;
     }
 }
